b0f693a7e6bc95e398727294a245a2e0f4ac5eb7
benjaminbalazs@gmail.com <benjaminbalazs@Benjamins-iMac-2.local>
2017-04-07 22:18:00 +0400

    cleanup

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ module.exports = {
 
     contentFor: function(type, config) {
 
-        if ( type === 'head-footer' ) {
+        if ( !process.env.EMBER_CLI_FASTBOOT ) {
 
-            if ( !process.env.EMBER_CLI_FASTBOOT ) {
+            if ( type === 'head-footer' ) {
 
                 var analytics = "\n<script>(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g; m.parentNode.insertBefore(a,m) })(window,document,'script','//www.google-analytics.com/analytics.js','ga');</script>";
                 var facebook = "\n<script>!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,document,'script','//connect.facebook.net/en_US/fbevents.js');</script>";
